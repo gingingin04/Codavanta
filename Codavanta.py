e551ddd17bed5de7588c55efda553972759d7377
gingingin04 <daingjee@gmail.com>
2025-09-20 18:53:56 +0200

    first commit

---
+++ Codavanta.py
@@ -0,0 +1,20 @@
+# Declaration and input of variables
+print("nigga")
+m = input('''Enter your mass
+          Mass is the All Up Weight, Total mass including batteries
+          : ''')
+
+
+# Hover Throttle % (Battery voltage Equation)
+
+# Thrust to Weight Ratio(Y)
+
+# Disk Loading (D)
+
+
+# Tip Mach value(Y)
+
+# Power Draw @ Hover and Cruising (D)
+
+
+# Motor Torque VS Prop Inertia match
