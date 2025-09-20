98330ebb05388d28fcb25a8ffd040de23899b84e
YIS8834 <y2005ihab@gmail.com>
2025-09-20 19:13:47 +0200

    Update Codavanta.py

--- Codavanta.py
+++ Codavanta.py
@@ -1,13 +1,20 @@
 # Declaration and input of variables
 print("nigga")
-m = input('''Enter your mass
+mass = input('''Enter your mass
           Mass is the All Up Weight, Total mass including batteries
           : ''')
-w = m * 9.81 #gravitational acceleration = 9.81
-
+weight = mass * 9.81 #gravitational acceleration = 9.81
+thrust50 = input('''Enter your thrust at 50% throttle
+          Please check the data sheet
+          : ''')
+thrust100 = input('''Enter your thrust at 100% throttle
+          Please check the data sheet
+          : ''')
 # Hover Throttle % (Battery voltage Equation)
 
 # Thrust to Weight Ratio(Y)
+thrustweightratio50 = thrust50/weight
+thrustweightratio100 = thrust100/weight
 
 # Disk Loading (D)
 
