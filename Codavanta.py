4fa036134881fc7a48bb5e401bb7444b1f2790e3
gingingin04 <daingjee@gmail.com>
2025-09-27 23:34:26 +0200

    ash

--- Codavanta.py
+++ Codavanta.py
@@ -6,6 +6,8 @@ pi = 22/7
 mass = float(input('''Enter your mass
           Mass is the All Up Weight, Total mass including batteries in Grams
           : '''))
+kg_mass = mass / 1000
+
 weight = mass * gravity  # gravitational acceleration = 9.81
 thrust50 = float(input('''Enter your thrust at 50% throttle
           Please check the data sheet
@@ -13,26 +15,27 @@ thrust50 = float(input('''Enter your thrust at 50% throttle
 thrust100 = float(input('''Enter your thrust at 100% throttle
           Please check the data sheet
           : '''))
-prop_dim = float(input('''Enter your prop size unit
+prop_unit = float(input('''Enter your prop size unit
                        1. in mm
                        2. in inches
                        : '''))
-   if prop_dim == 1:
-        prop_diameter = float(input('''Enter your propellor size in mm
-        : '''))
-    elif prop_dim == 2:
-        prop_diameter = float(input(''' Enter your propellor size in inches
-        :'''))
-    else:
-        print("you did not put in the right value")
 
-prop_num = float(input('''Enter your propellor number
+if prop_unit == 1:
+    print("prop unit will be calculated in mm")
+    prop_diameter = float(input("Enter prop diameter: "))
+elif prop_unit == 2:
+    print("prop unit will be calulctaed in inches")
+    prop_diameter = float(input("Enter prop diameter: "))
+else:
+    print("no clue what this is")
+
+prop_num = float(input('''Enter your propellor blade number
           : '''))
 
-print(mass)
+print(kg_mass)
 print(thrust50)
 print(thrust100)
-print(prop_diameter)
+print(prop_unit)
 
 
 # Hover Throttle % (Battery voltage Equation)
@@ -42,9 +45,18 @@ thrustweightratio50 = thrust50/weight
 thrustweightratio100 = thrust100/weight
 
 # Disk Loading (D)
-diskload = (mass * gravity) / ((prop_num * pi * (prop_diameter / 2) ** 2))
+
+mm_to_inch = 0.0393701
+
+if prop_unit == 1:
+    prop_diameter = prop_diameter
+else:
+    prop_diameter = prop_unit * 25.4
+
+diskload = (kg_mass * gravity) / ((prop_num * pi * (prop_diameter / 2) ** 2))
 
 print("the diskload is", diskload, "it is in grams per inch")
+print(prop_diameter)
 
 if 6 <= diskload <= 9:
     print("this is a freestyle quad")
