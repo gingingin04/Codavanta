c93fea1438b5c534e7022cbefd1115b67f14d2b6
gingingin04 <daingjee@gmail.com>
2025-09-27 16:45:59 +0200

    prop edits

--- Codavanta.py
+++ Codavanta.py
@@ -13,10 +13,26 @@ thrust50 = float(input('''Enter your thrust at 50% throttle
 thrust100 = float(input('''Enter your thrust at 100% throttle
           Please check the data sheet
           : '''))
+prop_dim = float(input('''Enter your prop size unit
+                       1. in mm
+                       2. in inches
+                       : '''))
+   if prop_dim == 1:
+        prop_diameter = float(input('''Enter your propellor size in mm
+        : '''))
+    elif prop_dim == 2:
+        prop_diameter = float(input(''' Enter your propellor size in inches
+        :'''))
+    else:
+        print("you did not put in the right value")
 
-prop_diameter = float(input('''Enter your propellor size in inches
-          At this stage, we are not taking into account the number of blades on the props
+prop_num = float(input('''Enter your propellor number
           : '''))
+
+print(mass)
+print(thrust50)
+print(thrust100)
+print(prop_diameter)
 
 
 # Hover Throttle % (Battery voltage Equation)
@@ -26,7 +42,15 @@ thrustweightratio50 = thrust50/weight
 thrustweightratio100 = thrust100/weight
 
 # Disk Loading (D)
-diskload = (mass * gravity) / ((4 * pi * (prop_diameter / 2) ** 2))
+diskload = (mass * gravity) / ((prop_num * pi * (prop_diameter / 2) ** 2))
+
+print("the diskload is", diskload, "it is in grams per inch")
+
+if 6 <= diskload <= 9:
+    print("this is a freestyle quad")
+
+else:
+    print("this is not a freestyle quad")
 
 # Tip Mach value(Y)
 
